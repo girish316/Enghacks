964f3f636d6708451a870cee2c4d6f9aa1069f0d
f47zhu <f47zhu@uwaterloo.ca>
2026-03-07 18:12:29 -0500

    Revert "Refactor frontend setup"
    This reverts commit 9b564eb7a92be9629c1d4cf7467c0739fca28dd9.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__, template_folder='client/build', static_folder='client/build/static')
+app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/heyyy")
+def hello_world():
+    print("girish is an attractive man")
