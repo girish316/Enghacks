a052b2afe829f91c64cf06c27cbbad1cffdd436a
Arulini Muthu <a2muthu@uwaterloo.ca>
2026-03-07 15:36:23 -0500

    included project adding feature on app.py

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/heyyy")
-def hello_world():
-    print("girish is an attractive man")
+projectsList = []
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html", projects = projectsList)
+
+@app.route("/addProject", methods = ["POST"])
+def addProject():
+    project = {
+                "title": request.form["title"],
+                "description": request.form["description"], 
+                "skills": request.form["skills"],  
+                "availability": request.form["availability"]
+               }
+    projects.append(project)
+    return redirect("/projects")
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+
